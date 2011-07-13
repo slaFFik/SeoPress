4f9a08d5c78d69022cb2b17e7ec6fea1d9727f25
wagesve <wagesve@rs-01>
2011-07-13 22:49:22 +0200

    Added actionhooks to post & page meta boxes
    Signed-off-by: wagesve <wagesve@rs-01>

--- admin/single_metabox.php
+++ admin/single_metabox.php
@@ -11,26 +11,36 @@ function sp_post_metabox(){
 	
 	$mb = new TK_WP_METABOX( 'sp_post_metabox', __( 'SEO Settings', 'seopress' ), 'post' );
 	
-	$html.= '<table class="form-table">
+	$title_field = apply_filters( 'sp_post_metabox_title', $title_field );
+	$description_field = apply_filters( 'sp_post_metabox_description', $description_field );
+	$keywords_field = apply_filters( 'sp_post_metabox_keywords', $keywords_field );
+	$noindex_field = apply_filters( 'sp_post_metabox_noindex', $noindex_field );
+	
+	$post_metabox_table = apply_filters( 'sp_post_metabox_table', $post_metabox_table );
+	
+	$html = '<table class="form-table">
 				<tbody>
 					<tr>
-						<td><label for="seopress_title">' . __( 'Title', 'seopress' ) . ':</label></td>
-						<td>' . tk_wp_form_textfield( 'title', 'sp_post_metabox', 'title',  ' style="width:99%"' ) . '</td>
+						<td width="200" valign="top"><label for="seopress_title">' . __( 'Title', 'seopress' ) . ':</label></td>
+						<td>' . tk_wp_form_textfield( 'title', 'sp_post_metabox', 'seopress_title',  ' style="width:99%"' ) . $title_field . '</td>
 					</tr>
 					<tr>
-						<td><label for="seopress_description">' . __( 'Description', 'seopress' ) . ':</label></td>
-						<td>' . tk_wp_form_textfield( 'description', 'sp_post_metabox', 'description', ' style="width:99%"' ) . '</td>
+						<td valign="top"><label for="seopress_description">' . __( 'Description', 'seopress' ) . ':</label></td>
+						<td>' . tk_wp_form_textfield( 'description', 'sp_post_metabox', 'seopress_description', ' style="width:99%"' ) . $description_field . '</td>
 					</tr>
 					<tr>
-						<td><label for="seopress_keywords">' . __( 'Keywords', 'seopress' ) . ':</label></td>
-						<td>' . tk_wp_form_textfield( 'keywords', 'sp_post_metabox', 'keywords', ' style="width:99%"' ) . '</td>
+						<td valign="top"><label for="seopress_keywords">' . __( 'Keywords', 'seopress' ) . ':</label></td>
+						<td>' . tk_wp_form_textfield( 'keywords', 'sp_post_metabox', 'seopress_keywords', ' style="width:99%"' ) . $keywords_field . '</td>
 					</tr>
 					<tr>
-						<td width="200"><label for="seopress_noindex">' . __( 'Ban searchengines', 'seopress' ) . ':</label></td>
-						<td>' . tk_wp_form_checkbox( 'noindex', 'sp_post_metabox', 'noindex' ) . '</td>
+						<td valign="top"><label for="seopress_noindex">' . __( 'Ban searchengines', 'seopress' ) . ':</label></td>
+						<td>' . tk_wp_form_checkbox( 'noindex', 'sp_post_metabox', 'seopress_noindex' ) . $noindex_field . '</td>
 					</tr>
+					' . $post_metabox_table .'
 				</tbody>
 			</table>';
+	
+	$html = apply_filters( 'sp_post_metabox_html', $html );
 	
 	$mb->add_element( $html );
 	
@@ -40,26 +50,36 @@ function sp_post_metabox(){
 function sp_page_metabox(){
 	$mb = new TK_WP_METABOX( 'sp_post_metabox', __( 'SEO Settings', 'seopress' ), 'page' );
 	
-	$html.= '<table class="form-table">
+	$title_field = apply_filters( 'sp_page_metabox_title', $title_field );
+	$description_field = apply_filters( 'sp_page_metabox_description', $description_field );
+	$keywords_field = apply_filters( 'sp_page_metabox_keywords', $keywords_field );
+	$noindex_field = apply_filters( 'sp_page_metabox_noindex', $noindex_field );
+	
+	$post_metabox_table = apply_filters( 'sp_page_metabox_table', $post_metabox_table );
+	
+	$html = '<table class="form-table">
 				<tbody>
 					<tr>
-						<td><label for="seopress_title">' . __( 'Title', 'seopress' ) . ':</label></td>
-						<td>' . tk_wp_form_textfield( 'title', 'sp_post_metabox', 'title',  ' style="width:99%"' ) . '</td>
+						<td width="200" valign="top"><label for="seopress_title">' . __( 'Title', 'seopress' ) . ':</label></td>
+						<td>' . tk_wp_form_textfield( 'title', 'sp_post_metabox', 'title',  ' style="width:99%"' ) . $title_field . '</td>
 					</tr>
 					<tr>
-						<td><label for="seopress_description">' . __( 'Description', 'seopress' ) . ':</label></td>
-						<td>' . tk_wp_form_textfield( 'description', 'sp_post_metabox', 'description', ' style="width:99%"' ) . '</td>
+						<td valign="top"><label for="seopress_description">' . __( 'Description', 'seopress' ) . ':</label></td>
+						<td>' . tk_wp_form_textfield( 'description', 'sp_post_metabox', 'description', ' style="width:99%"' ) . $description_field . '</td>
 					</tr>
 					<tr>
-						<td><label for="seopress_keywords">' . __( 'Keywords', 'seopress' ) . ':</label></td>
-						<td>' . tk_wp_form_textfield( 'keywords', 'sp_post_metabox', 'keywords', ' style="width:99%"' ) . '</td>
+						<td valign="top"><label for="seopress_keywords">' . __( 'Keywords', 'seopress' ) . ':</label></td>
+						<td>' . tk_wp_form_textfield( 'keywords', 'sp_post_metabox', 'keywords', ' style="width:99%"' ) . $keywords_field . '</td>
 					</tr>
 					<tr>
-						<td width="200"><label for="seopress_noindex">' . __( 'Ban searchengines', 'seopress' ) . ':</label></td>
-						<td>' . tk_wp_form_checkbox( 'noindex', 'sp_post_metabox', 'noindex' ) . '</td>
+						<td valign="top"><label for="seopress_noindex">' . __( 'Ban searchengines', 'seopress' ) . ':</label></td>
+						<td>' . tk_wp_form_checkbox( 'noindex', 'sp_post_metabox', 'noindex' ) . $noindex_field . '</td>
 					</tr>
+					' . $page_metabox_table .'
 				</tbody>
 			</table>';
+	
+	$html = apply_filters( 'sp_page_metabox_html', $html );
 	
 	$mb->add_element( $html );
 	
