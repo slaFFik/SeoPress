4384df8c25cbb660cb33cd707cba333682c3ddc5
wagesve <wagesve@rs-01>
2011-07-19 22:47:28 +0200

    Get pro
    - Added informations for pro version
    
    Signed-off-by: wagesve <wagesve@rs-01>

--- sp-core.php
+++ sp-core.php
@@ -312,7 +312,39 @@ function seopress_init(){
 }
 
 function sp_get_pro_tab( &$tabs ){
-	$tabs->add_tab( 'cap_get_pro', __ ('Get Pro version!', 'seopress'), __('Coming soon!', 'seopress') );	
+	global $seopress_plugin_url;
+	$html = '<div id="tab-head">
+	      <div class="sfb-entry">
+	      	  <div style="width:250px; padding:0 40px 100px 0; float:left;"><a href="http://wpplugins.com/plugin/238/seopress-pro-version" target="_blank"><img src="' . $seopress_plugin_url . 'images/seopress-pro-package.jpg" border="0" /></a></div>
+		      <h2>' . __('Pro Version now available!', 'seopress') . '</h2><br>
+				<b>' . __('Get SeoPress Pro Version now, and benefit from more functionality, support and a clean UI.', 'seopress') . '</b><br>
+				<br>
+				<a href="http://wpplugins.com/plugin/238/seopress-pro-version" target="_blank">' . __('Upgrade now', 'seopress') . '</a>					
+				<br><br>
+				<h3>' . __('Pro Features', 'seopress') . '</h3>
+				<ol>
+					<li>' . __('Keyword generator', 'seopress') . '</li>
+					<li>' . __('Text counter for meta title and description', 'seopress') . '</li>
+					<li>' . __('No branding: the Pro Version comes without the "Get Pro" tabs.', 'seopress') . '</li>
+					<li>' . __('Full support and help in the SeoPress group and forum.', 'seopress') . '</li>
+				</ol>
+				<br>
+				<h3>' . __('Pro Roadmap', 'seopress') . '</h3>
+				<ol>
+					<li>' . __( 'Sitemap generator (including Buddypress urls)', 'seopress') . '</li>
+					<li>' . __( 'Deeplink generator', 'seopress') . '</li>
+					<li>' . __( 'Pages and posts optimizer', 'seopress') . '</li>
+					<li>' . __( 'Searchengine preview for pages and posts', 'seopress') . '</li>
+					<li>' . __( 'Google Pagerank checker', 'seopress') . '</li>
+					<li>' . __( 'Canonical url support', 'seopress') . '</li>
+					<li>' . __( 'Xprofile special tags for buddypress', 'seopress') . '</li>
+				</ol>
+				<br>
+				<a href="http://wpplugins.com/plugin/238/seopress-pro-version" target="_blank">' . __('Upgrade now', 'seopress') . '</a>	
+			</div>
+	    </div>';
+	
+	$tabs->add_tab( 'cap_get_pro', __ ('Get Pro version!', 'seopress'), $html );	
 }
 
 function sp_reset_data(){
